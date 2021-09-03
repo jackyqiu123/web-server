314f759d14b1926453b0216bb221d50d5b2f7a8e
Jacky Qiu <jackyqiu@Jackys-MacBook-Air.local>
2021-09-03 13:06:12 -0700

    web server basic setup

--- WebServer.java
+++ WebServer.java
@@ -1,6 +1,19 @@
+import java.net.*;
+import java.io.*;
+
 public class WebServer {
-  public static void main(String[] args) {
-    // This file will be compiled by script and must be at 
-    // the root of your project directory
-  }
+  public static final int DEFAULT_PORT = 8080;
+
+  public static void main(String[] args) throws IOException{
+    ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
+    System.out.println("Server is running on port: " + DEFAULT_PORT);
+
+    while(true){
+      Socket clientSocket = serverSocket.accept();
+      System.err.println("Client connected");
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+    }
+    
+  } // end of main
 }
