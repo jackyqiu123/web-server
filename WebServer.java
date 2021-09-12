ab44a63475aee0ccfba8081f0f8803c38522835d
Lucas Carvajal <lcs.carvajal@gmail.com>
2021-09-12 11:50:30 -0700

    changes

--- WebServer.java
+++ WebServer.java
@@ -9,14 +9,12 @@ public class WebServer{
   public static final int DEFAULT_PORT = 8080;
   
   public static void main(String[] args) throws IOException{
-    // TODO: Start socket with port read in from httpd.conf file
-
-    ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
     final String HTTPD_CONF_FILE = "./config/httpd.conf";
     final String MIME_TYPES_FILE = "./config/mime.types";
     HttpdConf httpdConfig = new HttpdConf(HTTPD_CONF_FILE);
     MimeTypes mime = new MimeTypes(MIME_TYPES_FILE);
-    
+
+    ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
     System.out.println("Server is running on port: " + DEFAULT_PORT);
 
     while(true){
