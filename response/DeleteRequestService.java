de403df6edac2b51dc1a1607cd4446c1bf44beea
Jacky Qiu Xu <jaqiu123@gmail.com>
2021-09-14 23:09:10 -0700

    still working on deleteResponse

--- response/DeleteRequestService.java
+++ response/DeleteRequestService.java
@@ -3,6 +3,8 @@ package response;
 import request.Request;
 
 import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class DeleteRequestService {
     // possible responses are 202(Accepted), 204(No Content), 200(OK) for Delete request
@@ -10,12 +12,20 @@ public class DeleteRequestService {
     private FileReader fileReader;
     private static int statusCode;
     private String statusReason;
-
+    private String requestType;
+    private Socket socket;
+    private HashMap<String, String> headers;
     public DeleteRequestService(Request request) {
         this.uri = request.getUri();
-
+        this.requestType = request.getRequestType.toString();
+        this.headers = request.getHeaders();
     }
-    public String sendResponse(){}
+    public void sendResponse(){
+        BufferedWriter writer = new BufferedWrite(new OutputStreamWriter(socket.getOutputStream()));
+        String mime = headers.get("Content-Type");
+        String fileSize = headers.get("Content-Length");
+        String httpVersion = request.getHttpVersion();
+    }
 
     //TODO delete the file
     //TODO respond with response code by calling the ResponseService class
