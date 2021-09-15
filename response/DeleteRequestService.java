e4bab9934ccc16eb942d8042a2da7f5345ff2f97
Lucas Carvajal <lcs.carvajal@gmail.com>
2021-09-15 11:08:03 -0700

    fix DeleteRequestService

--- response/DeleteRequestService.java
+++ response/DeleteRequestService.java
@@ -2,29 +2,38 @@ package response;
 
 import request.Request;
 
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DeleteRequestService {
     // possible responses are 202(Accepted), 204(No Content), 200(OK) for Delete request
+    private Request request;
     private String uri;
     private FileReader fileReader;
     private static int statusCode;
     private String statusReason;
     private String requestType;
     private Socket socket;
-    private HashMap<String, String> headers;
+    private Map<String, String> headers;
     public DeleteRequestService(Request request) {
         this.uri = request.getUri();
-        this.requestType = request.getRequestType.toString();
+        this.requestType = request.getRequestType().toString();
         this.headers = request.getHeaders();
     }
+
     public void sendResponse(){
-        BufferedWriter writer = new BufferedWrite(new OutputStreamWriter(socket.getOutputStream()));
-        String mime = headers.get("Content-Type");
-        String fileSize = headers.get("Content-Length");
-        String httpVersion = request.getHttpVersion();
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+            String mime = headers.get("Content-Type");
+            String fileSize = headers.get("Content-Length");
+            String httpVersion = request.getHttpVersion();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //TODO delete the file
