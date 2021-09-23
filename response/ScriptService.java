da8a71aca85c0897f5b056068ba7eefda1493bca
Lucas Carvajal <lcs.carvajal@gmail.com>
2021-09-22 22:16:25 -0700

    start work on ScriptService

--- response/ScriptService.java
+++ response/ScriptService.java
@@ -1,7 +1,34 @@
 package response;
 
-public class ScriptService {
+import request.Request;
 
-    // TODO: execute script
+import java.io.BufferedWriter;
+import java.net.http.HttpClient;
+import java.util.Map;
+
+public class ScriptService {
+    private Request request;
+    private String scriptPath;
+    private BufferedWriter bufferedWriter;
+
+    public ScriptService(Request request, String scriptPath, BufferedWriter bufferedWriter) {
+        this.request = request;
+        this.scriptPath = scriptPath;
+    }
+
+    public void runScript() {
+        Map<String, String> headers = request.getHeaders();
+
+        ProcessBuilder processBuilder = new ProcessBuilder(scriptPath);
+        Map<String, String> environment = processBuilder.environment();
+
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            environment.put(entry.getKey(), entry.getValue());
+        }
+
+        processBuilder.redirectOutput();
+    }
+
+
 
 }
